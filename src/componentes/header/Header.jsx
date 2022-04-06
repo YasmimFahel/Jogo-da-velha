52bcdc953af67859fe23c47731ba6943aebb6d9b
YasmimFahel <y.s.fahel.2002@outlook.com>
2022-04-06 15:16:39 -0300

    Criado Componente de Ìcone

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -1,12 +1,16 @@
 import styles from './Header.module.css'
-import Subtitle from "../subtitle/Subtitle"
-import Title from "../title/Title"
+import Subtitle from '../subtitle/Subtitle'
+import Title from '../title/Title'
+import Icon from '../icon/Icon'
 
 function Header () {
     return(
        <div className={styles.header}>
         <Title>Jogo Da Velha</Title>
         <Subtitle>Criado por Yasmim Fahel</Subtitle>
+        <div className={styles.iconcontent}>
+            <Icon iconName="github" link="https://github.com/YasmimFahel" />
+        </div>
        </div>
     )
 }
